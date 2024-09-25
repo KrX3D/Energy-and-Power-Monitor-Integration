90b543d467e53b1e9d1aff16d6e42c55fd7fd568
KrX3D <KrX3D@users.noreply.github.com>
2024-09-25 13:12:57 +0200

    Create const.py

---
+++ const.py
@@ -0,0 +1,12 @@
+# custom_components/energy_power_monitor/const.py
+
+DOMAIN = "energy_power_monitor"
+
+CONF_ROOM = "room"
+CONF_SMART_MONITOR_DEVICE = "smart_monitor_device"
+CONF_ENTITIES = "entities"
+CONF_ENTITY_TYPE = "entity_type"
+CONF_INTEGRATION_ROOMS = "integration_rooms"
+
+ENTITY_TYPE_POWER = "power"
+ENTITY_TYPE_ENERGY = "energy"
