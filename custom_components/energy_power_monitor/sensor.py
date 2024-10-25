0f5d38f39544edac654d0faf3aa9f42847f20d83
KrX3D <KrX3D@users.noreply.github.com>
2024-10-25 02:18:56 +0200

    f

--- custom_components/energy_power_monitor/sensor.py
+++ custom_components/energy_power_monitor/sensor.py
@@ -84,6 +84,7 @@ def check_and_remove_nonexistent_entities(hass: HomeAssistant, entities, entry):
     #entry.data['entities'] = valid_entities
     _LOGGER.debug(f"Valid entities after check: {valid_entities}")
 
+
     return valid_entities
 
 class EnergyandPowerMonitorSensor(SensorEntity):
